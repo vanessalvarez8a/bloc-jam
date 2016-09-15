be5f3c3e092a24933055aceb1709b47a877bc2fd
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-09-14 21:13:24 -0500

    animated points

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,15 +1,37 @@
-var animatePoints = function() {
-    var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point'); //get points in selling points
 
-    var revealPoint = function() {
-      for(var i = 0; i < points.length; i++) {
-        points[i].style.opacity = 1;
-        points[i].style.transform = "scaleX(1) translateY(0)";
-        points[i].style.msTransform = "scaleX(1) translateY(0)";
-        points[i].style.WebkitTransform = "scaleX(1) translateY(0)"
-      }
+var animatePoints = function(points) { //injecting parameter where would animate every point
+
+    var revealPoint = function(index) { // function that applies styles to every point
+      points[index].style.opacity = 1;
+      points[index].style.transform = "scaleX(1) translateY(0)";
+      points[index].style.msTransform = "scaleX(1) translateY(0)";
+      points[index].style.WebkitTransform = "scaleX(1) translateY(0)"
+    };
+
+    for (var i = 0; i < pointsArray.length; i++) { //loop to pointsArray and apply the css style
+      revealPoint(i);
     }
-    revealPoint();
 }
 
-animatePoints();
+// animatePoints();
+
+window.onload = function() {
+
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+  window.addEventListener('scroll', function(event) { // this is the event handler also known as callback
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+
+    if(window.innerHeight > 950) {
+      animatePoints(pointsArray);
+    }
+    //get the distance in pixels from the window to the top of the container
+    // console.log('current offset from the top is' + sellingPoints.getBoundingClientRect().top + 'pixels')
+    // var sellingPoints = document.querySelector(".selling-points"); another way to use it without the need of [0] getElementsByClassName
+  });
+
+}
