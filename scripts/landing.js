306143d18d8603749e617b7ed13bb1a07c349c8c
vanessalvarez8a <vanessalvarez8a@gmail.com>
2016-10-17 20:50:26 -0500

    refactor landing.js with jquery

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,37 +1,31 @@
-var pointsArray = document.getElementsByClassName('point'); //get points in selling points
+var animatePoints = function() { //injecting parameter where would animate every point
 
-var animatePoints = function(points) { //injecting parameter where would animate every point
 
-    var revealPoint = function(index) { // function that applies styles to every point
-      points[index].style.opacity = 1;
-      points[index].style.transform = "scaleX(1) translateY(0)";
-      points[index].style.msTransform = "scaleX(1) translateY(0)";
-      points[index].style.WebkitTransform = "scaleX(1) translateY(0)"
-    };
-
-    for (var i = 0; i < pointsArray.length; i++) { //loop to pointsArray and apply the css style
-      revealPoint(i);
-    }
+      var revealPoint = function() { // function that applies styles to every point
+        $(this).css({
+          opacity: 1,
+          transform: "scaleX(1) translateY(0)"
+        });
+      }
+      $.each($('.point'), revealPoint);
 }
 
 // animatePoints();
 
-window.onload = function() {
+$(window).load(function() {
 
-  var sellingPoints = document.getElementsByClassName('selling-points')[0];
-  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  if($(window).height() > 950) {
+    animatePoints();
+  }
 
-  window.addEventListener('scroll', function(event) { // this is the event handler also known as callback
-    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
-      animatePoints(pointsArray);
-    }
+  var scrollDistance = $('.selling-points').offset().top - $(window).height() + 200;
+  // 200 pixeles abajo de selling point
 
-    if(window.innerHeight > 950) {
-      animatePoints(pointsArray);
+  $(window).scroll(function(event) {
+    if($(window).scrollTop() >= scrollDistance) {
+      animatePoints();
     }
     //get the distance in pixels from the window to the top of the container
-    // console.log('current offset from the top is' + sellingPoints.getBoundingClientRect().top + 'pixels')
-    // var sellingPoints = document.querySelector(".selling-points"); another way to use it without the need of [0] getElementsByClassName
   });
 
-}
+});
